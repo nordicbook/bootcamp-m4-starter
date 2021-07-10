8d2dfbac7516799774deee048db4a7da80eae06e
Aleksei Sokolov <aleksei.sokolov.85@mail.ru>
2021-07-10 10:10:10 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-
+// Стартовый файл 
 ReactDOM.render(
   <BrowserRouter>
     <App />
